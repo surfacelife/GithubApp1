0a4d64e5b879d23f44f6450219c7520baaf911bc
surfacelife <jakedotre@gmail.com>
2018-10-25 03:21:50 +0200

    Changed button text

--- GithubApp1/Form1.Designer.cs
+++ GithubApp1/Form1.Designer.cs
@@ -34,11 +34,12 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(258, 232);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(264, 142);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
